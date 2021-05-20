c144728f4f8f20ce7f877f589aae41cf72a12af3
Patricia <patrimdm96@gmail.com>
2021-05-20 17:09:00 +0200

    firebase configuration

---
+++ firebase/config.js
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+  apiKey: "AIzaSyAfkJqSA21Gm0U94XAXYrmQQMCemcu6l84",
+  authDomain: "product-hunt-nextjs-firebase.firebaseapp.com",
+  projectId: "product-hunt-nextjs-firebase",
+  storageBucket: "product-hunt-nextjs-firebase.appspot.com",
+  messagingSenderId: "785072371626",
+  appId: "1:785072371626:web:7bca74cf0347cb12f59dd3"
+};
+
+export default firebaseConfig
--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
+import App from 'next/app'
+import firebase, { FirebaseContext } from '../firebase'
+
 import '../styles/globals.css'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+// function MyApp({ Component, pageProps }) {
+//   return <Component {...pageProps} />
+// }
+
+const MyApp = ({ Component, pageProps }) => {
+  return (
+    <FirebaseContext.Provider value={{firebase}}>
+      <Component {...pageProps} />
+    </FirebaseContext.Provider>
+  )
 }
 
 export default MyApp
